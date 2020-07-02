2ef919b5365cd3d34fed3c833da79a4c4c5c9ae2
chandan kumar <chandankr.pra930@gmail.com>
2020-07-02 17:45:14 +0530

    after adding all folders related to seq, mongo, es6

--- Sequelize/models/database-connection/db.js
+++ Sequelize/models/database-connection/db.js
@@ -3,17 +3,25 @@ const Sequelize = require("sequelize");
 // dotenv.config();
 // const { POSTGRES_URI, POSTGRES_PASSWORD } = process.env;
 
-// console.log("uri : " + POSTGRES_URI +"\n"+ "pass : ",  POSTGRES_PASSWORD  +"\n")
+var pg = require('pg');
 
-// const sequelize = new Sequelize(
-//   POSTGRES_URI.replace("<password>", POSTGRES_PASSWORD)
-// );
-
-// const sequelize = new Sequelize('', 'username', 'password', {
-//   host: 'localhost',
-//   dialect:'postgres'
-// })
-const sequelize = new Sequelize('postgres://postgres:9308@localhost:5432/my_project_data')
+var conString = "	postgres://nuzlatgn:S6xgn_3p5Pw3YyaLHOHnL_M2OOYZqfQh@topsy.db.elephantsql.com:5432/nuzlatgn" //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log('WIth pg connection stabalized SUCCESSFULLY')
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    // client.end();
+  });
+});
+// const sequelize = new Sequelize('postgres://postgres:9308@localhost:5432/my_project_data')
 
 sequelize.sync();
 
